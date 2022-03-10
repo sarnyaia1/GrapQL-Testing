a3415f7c7b32952da17a7d50154210da756ab631
Sarnyai Ákos <sarnyaia1@gmail.com>
2022-03-10 12:25:56 +0100

    Testing Bahmutov's project

---
+++ todo-graphql-example-master/todo-graphql-example-master/db.js
@@ -0,0 +1,14 @@
+module.exports = {
+  todos: [
+    {
+      id: '1',
+      title: 'use GraphQL',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: 'write React frontend',
+      completed: false,
+    },
+  ],
+}
